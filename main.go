7dc12d5d91b38c865f20e416346292f840b87f55
administrator <administrator@bj11123.lan>
2019-10-20 00:33:14 +0800

    first commit

--- main.go
+++ main.go
@@ -11,12 +11,5 @@ import (
 func main() {
 	util.Logger.Info("start")
 	conf := config.NewConfig("config.cfg")
-	redis := datasource.NewRedisSouce(conf)
-	mongo := datasource.NewMongoSource(conf)
-	server2 := server.NewServer(conf, redis)
-	before := controller.NewBeforeClass(mongo, redis, "123")
-	in := controller.NewInClass(mongo, redis, nil , "123")
-	_ = server2.AddControl("/switcher/before", before)
-	_ = server2.AddControl("/switcher/in", in)
 	_ = server2.Start()
 }
